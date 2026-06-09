b5820401e1c5764ed9f4c0f2eeec04b3cae193ef
1cutecoder <820861340@qq.com>
2026-06-09 09:48:03 +0800

    feat: cache no store

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,7 @@
-export const dynamic = 'force-dynamic' // 动态更新 缓存组件不需要使用这个 默认都是动态内容
 export default async function Home() {
-  const randomImage = await fetch("https://www.loliapi.com/acg/pc?type=json"); //这个接口随机返回一个二刺猿图片
+  const randomImage = await fetch("https://www.loliapi.com/acg/pc?type=json", {
+    cache: "no-store",
+  }); //这个接口随机返回一个二刺猿图片
   const data = await randomImage.json();
   console.log(data);
   return (
