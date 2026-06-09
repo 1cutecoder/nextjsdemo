ef48d2859429f814e1dbc6d15c2917ab8b312e33
1cutecoder <820861340@qq.com>
2026-06-09 11:35:49 +0800

    feat: image import

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,10 @@
+import img1 from "@/public/1.png";
 import Image from "next/image";
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <Image src="/1.png" width={500} height={500} alt="1" />
+      <Image src={img1} width={192} height={108} alt="1" loading="eager" />
     </div>
   );
 }
