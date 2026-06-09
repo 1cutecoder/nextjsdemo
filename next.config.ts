ba6431583bc6d0c7a2750df07a70c85d89ab40f7
1cutecoder <820861340@qq.com>
2026-06-09 11:15:37 +0800

    feat: image component

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  // 缓存组件(关闭或者不配置)
-  cacheComponents: true,
   reactStrictMode: false
 };
 
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,18 +1,9 @@
-import { Suspense } from "react";
-
-const DynamicImage = async () => {
-  const randomImage = await fetch("https://www.loliapi.com/acg/pc?type=json"); //这个接口随机返回一个二刺猿图片
-  const data = await randomImage.json();
-  console.log(data);
-  return <img width={500} height={500} src={data.url} alt="random image" />;
-};
-export default async function Home() {
+import Image from "next/image";
+export default function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <Suspense fallback={<div>Loading...</div>}>
-        <DynamicImage />
-      </Suspense>
+      <Image src="/1.png" width={500} height={500} alt="1" />
     </div>
   );
 }
